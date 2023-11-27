24e0a44390e88c76ac7af6570e54e81e77094d70
mariyasada <mariyasada2406@gmail.com>
2023-11-27 14:39:03 +0530

    fix:optional chaining

--- src/compoenent/SingleCatDetails.jsx
+++ src/compoenent/SingleCatDetails.jsx
@@ -11,9 +11,9 @@ const SingleCatDetails = () => {
         <span>Name: {catDetail.name}</span>
         <span>Origin: {catDetail.origin}</span>
         <span>Description: {catDetail.description}</span>
-        <a href={catDetail.vcahospitals_url}>vcahospitals URL</a>
-        <a href={catDetail.vetstreet_url}>vetstreet URL</a>
-        <a href={catDetail.wikipedia_url}>Wikipedia URL</a>
+        <a href={catDetail?.vcahospitals_url}>vcahospitals URL</a>
+        <a href={catDetail?.vetstreet_url}>vetstreet URL</a>
+        <a href={catDetail?.wikipedia_url}>Wikipedia URL</a>
       </div>
     </div>
   );
